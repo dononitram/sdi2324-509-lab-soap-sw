8470a6b4f72180ec17e281d05204dd804bfb744d
dononitram <uo288787@uniovi.es>
2024-04-16 10:44:01 +0200

    SDI-2324-509-SW-11.1- Servicio SOAP Cliente.

---
+++ sdi2324-509-lab-soap-sw-client/src/main/java/com/uniovi/sdi/Sdi2324509LabSoapSwClientApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2324509LabSoapSwClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdi2324509LabSoapSwClientApplication.class, args);
+	}
+
+}
